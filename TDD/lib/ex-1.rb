cb535f55ca3ff87dafed1d280c0f1844d5ad4aca
Daniel Enriquez <daniel.enm17@gmail.com>
2023-05-09 17:49:38 -0600

    add TDD

--- TDD/lib/ex-1.rb
+++ TDD/lib/ex-1.rb
@@ -12,24 +12,65 @@
 # building the specs for that method, watch them fail, then write the code to
 # make them pass. Then move on to the other two methods, but this time you'll
 # create the descriptions to match the requirements above.
-
+require "rspec/autorun"
+require "pry"
 class Person
-  def initialize(first_name:, middle_name: nil, last_name:)
+  def initialize(first_name:, middle_name: "", last_name:)
     @first_name = first_name
     @middle_name = middle_name
     @last_name = last_name
   end
 
-  # implement your behavior here
+  def full_name
+    [@first_name, @middle_name, @last_name].compact.join(" ")
+  end
+  
+  def full_name_with_middle_initial
+    @middle_name = "#{@middle_name[0]}." if @middle_name
+    [@first_name, @middle_name, @last_name].compact.join(" ")
+  end
+
+  def initials
+    [@first_name[0], @middle_name[0], @last_name[0]].compact.join(".") + "."
+  end
 end
 
 RSpec.describe Person do
   describe "#full_name" do
-    it "concatenates first name, middle name, and last name with spaces"
-    it "does not add extra spaces if middle name is missing"
+    it "concatenates first name, middle name, and last name with spaces" do
+      person = Person.new(first_name: "John", middle_name: "Quincy", last_name: "Adams")
+      expect(person.full_name).to eq("John Quincy Adams")
+    end
+
+    it "does not add extra spaces if middle name is missing" do
+      person = Person.new(first_name: "John", last_name: "Adams")
+      expect(person.full_name).to eq("John Adams")
+    end
   end
 
-  describe "#full_name_with_middle_initial"
+  describe "#full_name_with_middle_initial" do
+    it "concatenates first name, middle name initial, and last name with spaces" do
+      person = Person.new(first_name: "John", middle_name: "Quincy", last_name: "Adams")
+      expect(person.full_name_with_middle_initial).to eq("John Q. Adams")
+    end
 
-  describe "#initials"
+    it "does not add extra spaces or a period if middle name is missing" do
+      person = Person.new(first_name: "John", last_name: "Adams")
+      expect(person.full_name_with_middle_initial).to eq("John Adams")
+    end
+  end
+
+  describe "#initials" do
+    it "returns the first letter of each name" do
+      person = Person.new(first_name: "John", middle_name: "Quincy", last_name: "Adams")
+      expect(person.initials).to eq("J.Q.A.")
+    end
+
+    it "returns the first letter of each name without extra periods if middle name is missing" do
+      person = Person.new(first_name: "John", last_name: "Adams")
+      expect(person.initials).to eq("J.A.")
+    end
+  end
 end
+
+
