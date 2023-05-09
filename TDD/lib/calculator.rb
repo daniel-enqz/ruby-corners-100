50187967abe81285cebdffb2c9ddccc071b0c844
Daniel Enriquez <daniel.enm17@gmail.com>
2023-05-09 16:38:03 -0600

    add TDD

---
+++ TDD/lib/calculator.rb
@@ -0,0 +1,44 @@
+require "rspec/autorun"
+
+class Calculator
+  def add(a, b)
+    a + b
+  end
+
+  def factorial(n)
+    if n == 0
+      1
+    else
+      (1..n).reduce(:*)
+    end
+  end
+end
+
+calc = Calculator.new
+calc.add(5, 10) # => 15
+
+describe Calculator do
+  describe "#add" do
+    it "returns the sum of its two arguments" do
+      calc = Calculator.new
+      expect(calc.add(5, 10)).to eq(15)
+    end
+
+    it "returns the sum of two different arguments" do
+      calc = Calculator.new
+      expect(calc.add(1, 2)).to eq(3)
+    end
+  end
+
+  describe "#factorial" do
+    it "returns 1 when given 0" do
+      clac = Calculator.new
+      expect(calc.factorial(0)).to eq(1)
+    end
+
+    it "returns 120 when given 5" do
+      calc = Calculator.new
+      expect(calc.factorial(5)).to eq(120)
+    end
+  end
+end
